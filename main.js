3a81156ec907e1f0708b51ab25971a485c08f403
Dennis A. Boanini <dennis.boanini01@gmail.com>
2022-06-24 19:01:10 +0200

    Challenge!

---
+++ main.js
@@ -0,0 +1,9 @@
+function mouseover() {
+    const element = document.getElementById('description');
+    element.style.fontSize = '1rem';
+}
+
+function mouseout() {
+    const element = document.getElementById('description');
+    element.style.fontSize = '1.1rem';
+}
